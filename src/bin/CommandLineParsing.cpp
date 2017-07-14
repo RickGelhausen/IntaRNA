52295a955c1a53f387f136600948424c1f2c771e
Martin Mann <martin-mann@users.noreply.github.com>
2017-07-14 15:36:49 +0200

    enable ES computation for pred.val = "m"

--- src/bin/CommandLineParsing.cpp
+++ src/bin/CommandLineParsing.cpp
@@ -1207,8 +1207,8 @@ getEnergyHandler( const Accessibility& accTarget, const ReverseAccessibility& ac
 {
 	checkIfParsed();
 
-	// check whether to compute ES values (for multi-site predictions
-	const bool initES = std::string("M").find(pred.val) != std::string::npos;
+	// check whether to compute ES values (for multi-site predictions)
+	const bool initES = std::string("Mm").find(pred.val) != std::string::npos;
 
 	switch( energy.val ) {
 	case 'B' : return new InteractionEnergyBasePair( accTarget, accQuery, tIntLoopMax.val, qIntLoopMax.val, initES );
