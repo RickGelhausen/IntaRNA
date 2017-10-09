877511ac1f8f41d377460dfdd0309bf2b9a0382d
RickGelhausen <rick.gelhausen@gmail.com>
2017-10-09 16:33:16 +0200

    Removed unwanted dimension from PredictorMfe4dMultiSeed. This should ensure a lower runtime compared to PredictorMfe4dMultiSimple.
    First draft of new PredictorMfe2dHeuristicSeed class. (not yet working)

--- src/IntaRNA/PredictorMfe4dMulti.cpp
+++ src/IntaRNA/PredictorMfe4dMulti.cpp
@@ -459,32 +459,32 @@ throw std::runtime_error("PredictorMfe4d::traceBack() : given interaction does n
         // Structure in S1
         if (traceNotFound && (allowES == ES_target || allowES == ES_xorQueryTarget)) {
             for (k1 = j1; traceNotFound && k1 > i1 + InteractionEnergy::minDistES; k1--) {
-                for (k2 = std::min(j2, i2 + energy.getMaxInternalLoopSize2() + 1); traceNotFound && k2 > i2; k2--) {
-                    if (hybridE(k1, k2) != NULL
-                        && hybridE(k1, k2)->size1() > (j1 - k1)
-                        && hybridE(k1, k2)->size2() > (j2 - k2))
-                    {
-                        if (E_equal(curE,
-                                    (energy.getE_multi(i1, k1, i2, k2, InteractionEnergy::ES_multi_mode::ES_multi_1only)
-                                     + (*hybridE(k1, k2))(j1 - k1, j2 - k2)
-                                    ))) {
-                            // stop searching
-                            traceNotFound = false;
-                            // store splitting base pair
-                            interaction.basePairs.push_back(energy.getBasePair(k1, k2));
-                            // store gap information
-                            if (interaction.gap == NULL) { interaction.gap = new Interaction::Gap(); }
-                            interaction.gap->energy += energy.getE_multi(i1, k1, i2, k2, InteractionEnergy::ES_multi_mode::ES_multi_1only);
-                            Interaction::BasePair bpLeft = energy.getBasePair(i1,i2);
-                            interaction.gap->gaps1.insert( IndexRange(bpLeft.first,interaction.basePairs.rbegin()->first) );
+			for (k2 = std::min(j2, i2 + energy.getMaxInternalLoopSize2() + 1); traceNotFound && k2 > i2; k2--) {
+				if (hybridE(k1, k2) != NULL
+					&& hybridE(k1, k2)->size1() > (j1 - k1)
+					&& hybridE(k1, k2)->size2() > (j2 - k2))
+				{
+					if (E_equal(curE,
+								(energy.getE_multi(i1, k1, i2, k2, InteractionEnergy::ES_multi_mode::ES_multi_1only)
+								 + (*hybridE(k1, k2))(j1 - k1, j2 - k2)
+								))) {
+						// stop searching
+						traceNotFound = false;
+						// store splitting base pair
+						interaction.basePairs.push_back(energy.getBasePair(k1, k2));
+						// store gap information
+						if (interaction.gap == NULL) { interaction.gap = new Interaction::Gap(); }
+						interaction.gap->energy += energy.getE_multi(i1, k1, i2, k2, InteractionEnergy::ES_multi_mode::ES_multi_1only);
+						Interaction::BasePair bpLeft = energy.getBasePair(i1,i2);
+						interaction.gap->gaps1.insert( IndexRange(bpLeft.first,interaction.basePairs.rbegin()->first) );
 
-                            // trace right part of split
-                            i1 = k1;
-                            i2 = k2;
-                            curE = (*hybridE(i1, i2))(j1 - i1, j2 - i2);
-                        }
-                    }
-                }
+						// trace right part of split
+						i1 = k1;
+						i2 = k2;
+						curE = (*hybridE(i1, i2))(j1 - i1, j2 - i2);
+					}
+				}
+			}
             }
         }
 
