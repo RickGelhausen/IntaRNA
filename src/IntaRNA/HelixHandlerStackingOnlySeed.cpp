6587232aa283f1adda92527829a728db13f8a94a
RickGelhausen <rick.gelhausen@gmail.com>
2018-03-23 14:58:15 +0100

    FIxed offset bug in HelixHandlerStackingOnlySeed.

--- src/IntaRNA/HelixHandlerStackingOnlySeed.cpp
+++ src/IntaRNA/HelixHandlerStackingOnlySeed.cpp
@@ -24,7 +24,7 @@ fillHelixSeed(const size_t i1min, const size_t i1max, const size_t i2min, const 
 	// fill for all start indices
 	// in increasing index order
 	for (i1=i1min; i1 < i1max+1; i1++ ) {
-	for (i2=i1min; i2 < i2max+1; i2++ ) {
+	for (i2=i2min; i2 < i2max+1; i2++ ) {
 		// count possible helices
 		helixCount++;
 
